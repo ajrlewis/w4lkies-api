04c853758fcacb6e0c1bb17bc7863493bb30fcb7
ajrlewis <hello@ajrlewis.com>
2025-04-26 17:48:53 +0100

    Added try except in verify password method.

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -11,17 +11,21 @@ from dependencies import GetDBDep
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(
-        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
-    )
+    try:
+        return bcrypt.checkpw(
+            plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+        )
+    except Exception as e:
+        logger.error(f"Error checking password: {e}")
+        return False
 
 
 def get_password_hash(password: str) -> str:
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode("utf-8"), salt).decode("utf-8")
 
 
 def authenticate_user(db: GetDBDep, name: str, password: str):
-    logger.debug(f"{db = } {name = } {password = }")
     user = user_crud.get_user_by_name(db, name)
     logger.debug(f"{user = }")
     if not user:
